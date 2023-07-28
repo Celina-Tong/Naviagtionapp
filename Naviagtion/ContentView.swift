922bad2eff9d18f42d45506428d2b600a668f929
Celina Tong <celinanatong@gmail.com>
2023-07-28 10:49:16 -0700

    Initial Commit

---
+++ Naviagtion/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Naviagtion
+//
+//  Created by Celina Tong on 7/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Naviagtion/NaviagtionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaviagtionApp.swift
+//  Naviagtion
+//
+//  Created by Celina Tong on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct NaviagtionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
